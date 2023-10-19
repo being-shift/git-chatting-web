ec246079310c0c6580523b187c4cebd5826fef07
being-shift <being_shift@icloud.com>
2023-10-19 20:27:15 +0900

    update entire login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
+import {useEffect, useState} from "react";
 import "./App.css";
 import socket from "./server";
 
 function App() {
-  return (
-    <div>
-      <div className="App"></div>
-    </div>
-  );
+	const [user, setUser] = useState(null);
+	useEffect(()=> {askUserName();},[]);
+
+	const askUserName = () => {
+		const userName = prompt("당신의 이름을 입력하세요");
+		console.log("uuuu",userName);
+
+		socket.emit("login",userName,(res)=>{
+			console.log("Res",res);
+			if(res?.ok){
+				setUser(res.data);
+			}
+		});
+	}
+
+	return (
+		<div>
+		  <div className="App"></div>
+		</div>
+	);
 }
 
 export default App;

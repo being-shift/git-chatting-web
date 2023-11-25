6182d2df4e4690f856c35b3adcfb5adf6f64982c
being-shift <being_shift@icloud.com>
2023-11-25 21:01:23 +0900

    강의내용 모두 완료버전

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import {useEffect, useState} from "react";
 import "./App.css";
 import socket from "./server";
+import InputField from "./components/InputField/InputField";
+import MessageContainer from "./components/MessageContainer/MessageContainer";
 
 function App() {
 	const [user, setUser] = useState(null);
-	useEffect(()=> {askUserName();},[]);
+	const [message, setMessage] = useState('');
+	const [messageList, setMessageList] = useState([]);
+	console.log(new Date(), "message List", messageList);
+	useEffect(()=> {
+		socket.on("message", (message) => {
+				console.log(new Date(), "all got message : ", message);
+			setMessageList((prevState) => prevState.concat(message));
+		});
+		askUserName();
+	},[]);
 
 	const askUserName = () => {
 		const userName = prompt("당신의 이름을 입력하세요");
-		console.log("uuuu",userName);
+		console.log(new Date(), "uuuu",userName);
 
 		socket.emit("login",userName,(res)=>{
-			console.log("Res",res);
+			console.log(new Date(), "Res",res);
 			if(res?.ok){
 				setUser(res.data);
 			}
 		});
 	}
-
+	const sendMessage = (event) => {
+		console.log(new Date(), event.preventDefault());
+		socket.emit("sendMessage", message, (res) => {
+			console.log(new Date(), "sendMessage res : ", res);
+		});
+	}	
 	return (
 		<div>
-		  <div className="App"></div>
+			<div className="App">
+				<MessageContainer messageList={messageList} user={user} />
+				<InputField message={message} setMessage={setMessage} sendMessage={sendMessage} />
+			</div>
 		</div>
 	);
 }
